d87ded4ff0db5389737a55afcf38910f13e45614
Khalil <khalilpati55@gmail.com>
2024-08-06 18:03:45 +0530

    Scaffold React project with Tailwind using Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="container max-w-xl mx-auto my-4 flex flex-col gap-4">
+      <h1 className="font-bold text-3xl p-4 border rounded text-center">
+        React Query
+      </h1>
+    </main>
+  );
+}
+
+export default App;
